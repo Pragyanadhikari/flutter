ea0f42168d1a30eb6e7f99fd02aeb091c7742c37
Pragyan Adhikari <pragyanadhikari961@gmail.com>
2024-06-30 17:19:36 +0545

    Changes

---
+++ lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:coffe_project/coffee_prefs.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "My coffee Id",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: const Color.fromARGB(255, 159, 111, 97),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.amber[700],
+            padding: const EdgeInsets.all(33),
+            child: const Text("Hello I like my coffee..."),
+          ),
+          Container(
+            color: Colors.blueGrey[900],
+            padding: const EdgeInsets.all(33),
+            child: const CoffeePrefs() ,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,77 @@
+import 'package:coffe_project/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("My coffee Idea"),
-          backgroundColor: Color.fromARGB(255, 159, 111, 97),
-          centerTitle: true,
-        ),
-        body: const Home(),
-      ),
-    ),
+    const MaterialApp(home: Home()),
   );
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello , Pragyan");
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("SandBox"),
+          backgroundColor: Colors.grey,
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 100,
+              color: Colors.red,
+              child: const Text("One"),
+            ),
+            Container(
+              height: 200,
+              color: Colors.blueAccent,
+              child: const Text("Two"),
+            ),
+            Container(
+              height: 300,
+              color: Colors.greenAccent,
+              child: const Text("Three"),
+            ),
+          ],
+        ));
   }
 }
+
+    
+    
+    
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    // Container(
+    //   color: Colors.orange,
+    //   // width: 200,
+    //   // height: 100,
+    //   padding: const EdgeInsets.all(10),
+    //   margin: const EdgeInsets.fromLTRB(10, 20, 30, 40),
+    //   child: const Text(
+    //     'Hello,Pragyan',
+    //     style: TextStyle(
+    //       fontSize: 14,
+    //       letterSpacing: 4,
+    //       decoration: TextDecoration.underline,
+    //       fontStyle: FontStyle.italic,
+    //     ),
+    //   ),
+    // );
+
